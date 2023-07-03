a6fbf4c47e75256c218f31718b6b648f1a9e6d1c
cool-Moshood <moshoodage@gmail.com>
2023-07-03 02:51:00 +0100

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task2 = true;
-    let task = false;
+    const task = false;
+    const task2 = true;
+    return [task, task2];
   }
 
   return [task, task2];
