40cb81f46aea88c7c9fbda29314adce644c787a0
cool-Moshood <moshoodage@gmail.com>
2023-07-03 01:41:46 +0100

    task 8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,11 @@
+const getCurrentYear = () => new Date().getFullYear();
+
+const getBudgetForCurrentYear = (income, gdp, capita) => {
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita
+  };
+
+  return budget;
+};
